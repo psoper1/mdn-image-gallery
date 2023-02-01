d1b0e54f0a31996aa4cb555f7259e1120eac4444
reeman9000 <psoper0604@gmail.com>
2023-02-01 16:59:56 +0000

    pasted the darken thing because I couldn't figure it out

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
 
-const images = ["/images/pic1.jpg", "/images/pic2.jpg", "pic3.jpg", "pic4.jpg", "pic5.jpg"];
+const images = ["pic1.jpg", "pic2.jpg", "pic3.jpg", "pic4.jpg", "pic5.jpg"];
 
 /* Declaring the alternative text for each image file */
 
@@ -33,3 +33,16 @@ for (const image of images) {
 }
 
 /* Wiring up the Darken/Lighten button */
+
+btn.addEventListener('click', () => {
+    const btnClass = btn.getAttribute('class');
+    if (btnClass === 'dark') {
+      btn.setAttribute('class','light');
+      btn.textContent = 'Lighten';
+      overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
+    } else {
+      btn.setAttribute('class','dark');
+      btn.textContent = 'Darken';
+      overlay.style.backgroundColor = 'rgba(0,0,0,0)';
+    }
+  });
