7b9c590e607e6afe6efe655cba1a7a6aad71c442
reeman9000 <psoper0604@gmail.com>
2023-02-01 16:46:12 +0000

    added images to js file

--- main.js
+++ main.js
@@ -6,6 +6,15 @@ const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
 
+const images = ["pic1.jpg", "pic2.jpg", "pic3.jpg", "pic4.jpg", "pic5.jpg"];
+const alts = {
+    'pic1.jpg' : 'Closeup of a an eye',
+    'pic2.jpg' : 'Rock? Sand?',
+    'pic3.jpg' : 'White and purple flowers',
+    "pic4.jpg" : "Egyptian Hyroglyphs"
+    "pic5.jpg" : "Moth on a leaf"
+}
+
 /* Declaring the alternative text for each image file */
 
 /* Looping through images */
