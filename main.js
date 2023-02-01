be5202c1353f6304c81f498046999d16d48784a0
reeman9000 <psoper0604@gmail.com>
2023-02-01 16:57:17 +0000

    only 3 pictures work

--- main.js
+++ main.js
@@ -6,16 +6,16 @@ const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
 
-const images = ["pic1.jpg", "pic2.jpg", "pic3.jpg", "pic4.jpg", "pic5.jpg"];
+const images = ["/images/pic1.jpg", "/images/pic2.jpg", "pic3.jpg", "pic4.jpg", "pic5.jpg"];
 
 /* Declaring the alternative text for each image file */
 
 const alts = {
-    "pic1.jpg": "Closeup of a an eye",
-    "pic2.jpg": "Rock? Sand?",
-    "pic3.jpg": "White and purple flowers",
-    "pic4.jpg": "Egyptian Hyroglyphs",
-    "pic5.jpg": "Moth on a leaf"
+    "/images/pic1.jpg": "Closeup of a an eye",
+    "/images/pic2.jpg": "Rock? Sand?",
+    "/images/pic3.jpg": "White and purple flowers",
+    "/images/pic4.jpg": "Egyptian Hyroglyphs",
+    "/images/pic5.jpg": "Moth on a leaf"
 }
 
 /* Looping through images */
@@ -23,7 +23,7 @@ const alts = {
 for (const image of images) {
 
     const newImage = document.createElement("img");
-    newImage.setAttribute("src", `images/$(image)`);
+    newImage.setAttribute("src", `images/${image}`);
     newImage.setAttribute("alt", alts[image]);
     thumbBar.appendChild(newImage);
     newImage.addEventListener("click", e => {
@@ -31,10 +31,5 @@ for (const image of images) {
         displayedImage.alt = e.target.alt;
     });
 }
-
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-newImage.setAttribute('alt', xxx);
-thumbBar.appendChild(newImage);
 
 /* Wiring up the Darken/Lighten button */
